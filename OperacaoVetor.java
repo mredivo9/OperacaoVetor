62d907e725749b5da5a6291b5082290ce9f86769
mredivo9 <168040123+mredivo9@users.noreply.github.com>
2024-09-08 21:06:56 -0300

    Add files via upload

---
+++ OperacaoVetor.java
@@ -0,0 +1,10 @@
+public class OperacaoVetor {
+    public static void main(String[] args) {
+        int[] vetor = {3, 6, 9, 12};
+
+        int soma = vetor[0] + vetor[vetor.length - 1];
+
+        System.out.println("A soma do primeiro e do último elemento é: " + soma);
+
+    }
+}
